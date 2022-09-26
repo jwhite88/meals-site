9607ffb011979321d60ea50546cf2aaba3734072
jwhite88 <74882417+jwhite88@users.noreply.github.com>
2022-09-26 12:34:05 -0400

    Added styles to categories

--- src/components/Category.js
+++ src/components/Category.js
@@ -13,7 +13,7 @@ function Category() {
     useEffect(() => {
         axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${params.categoryId}`)
         .then((meals) => {  
-            setCount(10);
+            setCount(12);
             setMax(meals.data.meals.length)
             setRecipes(meals.data.meals);
         })
@@ -35,23 +35,31 @@ function Category() {
       }
   return (
     <div>
-      <section>
-        <h1>Category</h1>
-        <div>
+      <section className='container mx-auto'>
+        <h1 className='directions-ingredients text-2xl py-4'>{params.categoryId} Recipes</h1>
+        <div className='flex flex-row flex-wrap justify-around gap-8'>
           {Array.isArray(recipes) &&
             recipes.slice(0, count).map((recipe) => {
               return (
                 <div key={recipe.idMeal}>
-                  <h3>{recipe.strMeal}</h3>
-                  <img className='w-[170px] block' src={recipe.strMealThumb} alt={recipe.strMeal} />
-                  {recipe.idMeal}
-                 <Link to={`/${params.categoryId}/${recipe.idMeal}`}>Recipe</Link>
+                  <h3 className='pb-1 text-lg pl-2'>{recipe.strMeal.substring(0,25)}{recipe.strMeal.length > 25 ? "..." : ""}</h3>
+                  <img
+                    className='w-[250px] block rounded-md'
+                    src={recipe.strMealThumb}
+                    alt={recipe.strMeal}
+                  />
+                  {/* {recipe.idMeal} */}
+                  <Link to={`/${params.categoryId}/${recipe.idMeal}`}>
+                    <span className='inline-block mt-2 pt-02 border border-2 border-emerald-800 min-w-[5rem] rounded-md text-center'>
+                      Recipe
+                    </span>
+                  </Link>
                 </div>
               );
             })}
         </div>
+        {count < max && <button className='block border-solid border-[1.75px] border-teal-800 rounded-md min-w-[8rem] mt-4 text-xl' onClick={loadMore}>Load More</button>}
       </section>
-      {count < max && <button onClick={loadMore}>Load More</button>}
     </div>
   );
 }
